c95fdabd9aa206b0923645c48a3a4ceee2666148
sevajal <sevajal@yahoo.com>
2023-01-10 20:23:01 -0500

    Second draft

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -7,25 +7,4 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 
-/**
- * print_grid - Print 3x3 grid
- * @grid: 3x3 grid
- *
- */
-static void print_grid(int grid[3][3])
-{
-    int i, j;
-
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
-}
-
 #endif
