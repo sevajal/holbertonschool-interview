951c26cca70c8f1d6f8371e38e9ed15aef62ebdf
sevajal <sevajal@yahoo.com>
2023-01-17 22:50:27 -0500

    First draft

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: Pointer to the listint_t head.
+ * Return: the number of elements in a linked listint_t list.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int nodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
+
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: Double pointer to the head of the linked list.
+ * An empty list is considered a palindrome.
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int length = listint_len(*head);
+	int numbers[length];
+	int x, i = 0;
+	listint_t *tmp = *head;
+
+	while (tmp != NULL)
+	{
+		numbers[i] = tmp->n;
+		tmp = tmp->next;
+		/*printf("number[%d]=%d\n", i, numbers[i]);*/
+		i++;
+	}
+	/*printf("%i", length);*/
+
+	x = length - 1;
+	i = 0;
+	while (i <= length / 2)
+		if (numbers[i] == numbers[x])
+		{
+			i++;
+			x--;
+		}
+		else
+			return (0);
+	return (1);
+}
