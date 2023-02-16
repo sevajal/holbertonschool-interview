a7492096906dcd7f8a5802a2d80aef9355896f8b
sevajal <sevajal@yahoo.com>
2023-02-15 22:45:30 -0500

    indentation

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -12,12 +12,12 @@ def validUTF8(data):
                 if bit == '0':
                     break
                 n_bytes += 1
-                if n_bytes == 0:
-                    continue
-                if n_bytes == 1 or n_bytes > 4:
-                    return False
-            else:
-                if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
-                    return False
+            if n_bytes == 0:
+                continue
+            if n_bytes == 1 or n_bytes > 4:
+                return False
+        else:
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+                return False
         n_bytes -= 1
     return True
