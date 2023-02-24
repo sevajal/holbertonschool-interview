5ed480a45d1187f64f22ffdaf6ea1a15904218fe
sevajal <sevajal@yahoo.com>
2023-02-23 23:56:27 -0500

    final draft

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,105 +1,71 @@
 #include "slide_line.h"
 
 /**
- * slide_left - Given an lineay of integers, we want to be able to slide
- * & merge it to the left.
- * @line: Pointer to an lineay of integer containing size elements.
- * @size: The size of the lineay.
- * Return: 1 if success or 0 if failure.
+ * slide_line - Slide and merge an array of integers to the left or right
+ * @line: Pointer to an array of integers
+ * @size: Size of the array
+ * @direction: Direction to slide and merge the array (SLIDE_LEFT or SLIDE_RIGHT)
+ *
+ * Return: 1 upon success, or 0 upon failure
  */
-
-int slide_left(int *line, size_t size)
-{
-	size_t i, j, k;
-    /* Slide non-zero elements to the left */
-	for (i = 0, j = 0; i < size; i++)
-	{
-		if (line[i] != 0)
-			line[j++] = line[i];
-	}
-	/* Merge contiguous or separated identical numbers */
-	for (i = 0; i < j; i = k)
-	{
-		k = i + 1;
-		while (k < j && line[k] == line[i])
-		{
-			line[i] *= 2;
-			line[k++] = 0;
-		}
-	}
-	/* Slide merged elements to the left */
-	for (i = 0, j = 0; i < j || j < size; )
-	{
-		while (i < size && line[i] == 0)
-			i++;
-		while (j < size && line[j] != 0)
-			j++;
-		if (i < j && j < size)
-			line[i++] = line[j++];
-	}
-	/* Fill the rest with zeros */
-	while (i < size)
-		line[i++] = 0;
-	return (1);
-}
-
-/**
- * slide_right - Given an lineay of integers, we want to be able to slide
- * & merge it to the right.
- * @line: Pointer to an lineay of integer containing size elements.
- * @size: The size of the lineay.
- * Return: 1 if success or 0 if failure.
- */
-
-int slide_right(int *line, size_t size)
-{
-	size_t i, j, k;
-    /* Slide non-zero elements to the right */
-	for (i = size - 1, j = size - 1; i < size; i--)
-	{
-		if (line[i] != 0)
-			line[j--] = line[i];
-	}
-	/* Merge contiguous or separated identical numbers */
-	for (i = size - 1; i < size; i = k)
-	{
-		k = i - 1;
-		while (line[k] == line[i])
-		{
-			line[i] *= 2;
-			line[k--] = 0;
-		}
-	}
-	/* Slide merged elements to the right */
-	for (i = size - 1, j = size - 1; i > j; )
-	{
-		while (line[i] == 0)
-			i--;
-		while (line[j] != 0)
-			j--;
-		if (i > j)
-			line[j--] = line[i--];
-	}
-	/* Fill the rest with zeros */
-	while (j < i)
-		line[j--] = 0;
-	return (1);
-}
-
-/**
- * slide_line - Given an lineay of integers, we want to be able to slide
- * & merge it to the left or to the right.
- * @line: Pointer to an lineay of integer containing size elements.
- * @size: The size of the lineay.
- * @direction: SLIDE_LEFT or SLIDE_RIGHT
- * Return: 1 if success or 0 if failure.
- */
-
 int slide_line(int *line, size_t size, int direction)
 {
-	if (direction != SLIDE_RIGHT || direction != SLIDE_LEFT)
+	int i, j, k, last_merged;
+
+	if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
 		return (0);
-	if (direction == SLIDE_RIGHT)
-		return (slide_right(line, size));
-	return (slide_left(line, size));
+	last_merged = 0;
+	for (i = 0; i < (int)size; i++)
+	{
+		if (line[i] == 0)
+			continue;
+		if (last_merged)
+			last_merged = 0;
+		for (j = i + 1; j < (int)size; j++)
+		{
+			if (line[j] == 0)
+				continue;
+			if (line[j] != line[i])
+				break;
+			line[i] *= 2;
+			line[j] = 0;
+			last_merged = 1;
+			break;
+		}
+	}
+	if (direction == SLIDE_LEFT)
+	{
+		k = 0;
+		for (i = 0; i < (int)size; i++)
+		{
+			if (line[i] == 0)
+				continue;
+			if (k != i)
+			{
+				line[k] = line[i];
+				line[i] = 0;
+			}
+			k++;
+			if (last_merged && k < (int)size)
+				last_merged = 0;
+		}
+	}
+	else
+	{
+		k = (int)size - 1;
+		for (i = (int)size - 1; i >= 0; i--)
+		{
+			if (line[i] == 0)
+				continue;
+			if (k != i)
+			{
+				line[k] = line[i];
+				line[i] = 0;
+			}
+			k--;
+			if (last_merged && k >= 0)
+				last_merged = 0;
+		}
+	}
+	return (1);
 }
--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -6,7 +6,5 @@
 #define SLIDE_LEFT 0
 
 int slide_line(int *line, size_t size, int direction);
-int slide_left(int *line, size_t size);
-int slide_right(int *line, size_t size);
 
 #endif
