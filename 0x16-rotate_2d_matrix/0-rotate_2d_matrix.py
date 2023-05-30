9361ef141cd876029d809e940f09870e74e39d79
sevajal <sevajal@yahoo.com>
2023-05-29 19:12:58 -0500

    Task 0

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,8 +5,8 @@
 def rotate_2d_matrix(matrix):
     """ Function that rotates a 2d matrix 90 degrees clockwise """
     matrix.reverse()
-    m = [i.copy()
+    matrixCopy = [i.copy()
          for i in matrix]
-    for i, row in enumerate(m):
+    for i, row in enumerate(matrixCopy):
         for j, col in enumerate(row):
-            matrix[j][i] = m[i][j]
+            matrix[j][i] = matrixCopy[i][j]
